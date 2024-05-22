9897d2bfa639168f1f720d856ed3bca6f0ccfe9d
Jacob Bello <vitrollers@outlook.com>
2024-05-21 23:44:55 -0500

    📦 NEW: my changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next()],
   output: "server",
+  adapter: deno(),
 });
