f40339dffbb26735ae48b9fd04b3758acfbc6199
Jacob Bello <vitrollers@outlook.com>
2024-05-22 00:04:33 -0500

    📦 NEW: my changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import deno from "@deno/astro-adapter";
+import sharpPlugin from "vite-plugin-sharp";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next()],
   output: "server",
   adapter: deno(),
+  site: "https://jackbello.deno.dev",
+  vite: {
+    plugins: [sharpPlugin()],
+  },
 });
